955b9c6ab4dd0b8c8619534b2777a5782a7143bf
yufree <yufree@live.cn>
2020-03-11 14:41:31 -0400

    Clean commit

---
+++ R/data.R
@@ -0,0 +1,55 @@
+#' Demo mzrt list object
+#' @format mzrt list object
+#' @usage data(mzrt)
+"mzrt"
+
+#' Demo ipo parameters object
+#' @format ipo parameters
+#' @usage data(para)
+"para"
+
+#' Demo xcmseic object
+#' @format xcmseic object
+#' @usage data(srmeic)
+"srmeic"
+
+#' Demo xcmsset object
+#' @format xcmsset object
+#' @usage data(srmxset)
+"srmxset"
+
+#' A list containing HMDB qqq MS/MS data with peaks larger than 10 percentage for PMD annatation
+#' @docType data
+#' @usage data(qqq)
+#' @format A list containing HMDB qqq MS/MS data with peaks larger than 10 percentage for PMD annatation
+#' \describe{
+#'   \item{name}{HMDB ID}
+#'   \item{mz}{mass to charge ratio}
+#'   \item{msms}{msms pmd}
+#'   \item{msmsraw}{raw msms data}
+#'   }
+"qqq"
+
+#' A list containing HMDB qtof MS/MS data with peaks larger than 10 percentage for PMD annatation
+#' @docType data
+#' @usage data(qtof)
+#' @format A list containing HMDB qtof MS/MS data with peaks larger than 10 percentage for PMD annatation
+#' \describe{
+#'   \item{name}{HMDB ID}
+#'   \item{mz}{mass to charge ratio}
+#'   \item{msms}{msms pmd}
+#'   \item{msmsraw}{raw msms data}
+#'   }
+"qtof"
+
+#' A list containing HMDB orbitrap MS/MS data with peaks larger than 10 percentage for PMD annatation
+#' @docType data
+#' @usage data(orb)
+#' @format A list containing HMDB orbitrap MS/MS data with peaks larger than 10 percentage for PMD annatation
+#' \describe{
+#'   \item{name}{HMDB ID}
+#'   \item{mz}{mass to charge ratio}
+#'   \item{msms}{msms pmd}
+#'   \item{msmsraw}{raw msms data}
+#'   }
+"orb"
