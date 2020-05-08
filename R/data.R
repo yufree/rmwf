7f226fdcf1e2558daeaa83ec9db0d30756fab5fd
yufree <yufree@live.cn>
2020-05-08 19:53:57 -0400

    update package

--- R/data.R
+++ R/data.R
@@ -3,20 +3,40 @@
 #' @usage data(mzrt)
 "mzrt"
 
+#' Demo mzrt list object for negative mode
+#' @format mzrt list object
+#' @usage data(mzrtn)
+"mzrtn"
+
 #' Demo ipo parameters object
 #' @format ipo parameters
 #' @usage data(para)
 "para"
+
+#' Demo ipo parameters object for negative mode
+#' @format ipo parameters
+#' @usage data(paran)
+"paran"
 
 #' Demo xcmseic object
 #' @format xcmseic object
 #' @usage data(srmeic)
 "srmeic"
 
+#' Demo xcmseic object for negative mode
+#' @format xcmseic object
+#' @usage data(srmnegeic)
+"srmnegeic"
+
 #' Demo xcmsset object
 #' @format xcmsset object
 #' @usage data(srmxset)
 "srmxset"
+
+#' Demo xcmsset object for negative mode
+#' @format xcmsset object
+#' @usage data(srmnegxset)
+"srmnegxset"
 
 #' A list containing HMDB qqq MS/MS data with peaks larger than 10 percentage for PMD annatation
 #' @docType data
