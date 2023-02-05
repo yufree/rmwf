322e853ed1c92dd0899a9c807b2d0582d0c93844
yufree <yufree@live.cn>
2023-02-05 00:11:58 -0500

    update

--- R/data.R
+++ R/data.R
@@ -1,46 +1,45 @@
 #' Demo mzrt list object
+#' @docType data
 #' @format mzrt list object
-#' @usage data(mzrt)
 "mzrt"
 
 #' Demo mzrt list object for negative mode
+#' @docType data
 #' @format mzrt list object
-#' @usage data(mzrtn)
 "mzrtn"
 
 #' Demo ipo parameters object
+#' @docType data
 #' @format ipo parameters
-#' @usage data(para)
 "para"
 
 #' Demo ipo parameters object for negative mode
+#' @docType data
 #' @format ipo parameters
-#' @usage data(paran)
 "paran"
 
 #' Demo xcmseic object
+#' @docType data
 #' @format xcmseic object
-#' @usage data(srmeic)
 "srmeic"
 
 #' Demo xcmseic object for negative mode
+#' @docType data
 #' @format xcmseic object
-#' @usage data(srmneic)
 "srmneic"
 
 #' Demo xcmsset object
+#' @docType data
 #' @format xcmsset object
-#' @usage data(srmxset)
 "srmxset"
 
 #' Demo xcmsset object for negative mode
+#' @docType data
 #' @format xcmsset object
-#' @usage data(srmnxset)
 "srmnxset"
 
 #' A list containing HMDB qqq MS/MS data with peaks larger than 10 percentage for PMD annotation
 #' @docType data
-#' @usage data(qqq)
 #' @format A list containing HMDB qqq MS/MS data with peaks larger than 10 percentage for PMD annotation
 #' \describe{
 #'   \item{name}{HMDB ID}
@@ -52,7 +51,6 @@
 
 #' A list containing HMDB qtof MS/MS data with peaks larger than 10 percentage for PMD annotation
 #' @docType data
-#' @usage data(qtof)
 #' @format A list containing HMDB qtof MS/MS data with peaks larger than 10 percentage for PMD annotation
 #' \describe{
 #'   \item{name}{HMDB ID}
@@ -64,7 +62,6 @@
 
 #' A list containing HMDB orbitrap MS/MS data with peaks larger than 10 percentage for PMD annotation
 #' @docType data
-#' @usage data(orb)
 #' @format A list containing HMDB orbitrap MS/MS data with peaks larger than 10 percentage for PMD annotation
 #' \describe{
 #'   \item{name}{HMDB ID}
@@ -76,7 +73,6 @@
 
 #' A data frame with compounds from hmdb and refmet
 #' @docType data
-#' @usage data(hr)
 #' @format A data frame with compounds from hmdb and refmet including name, InChIKey, chemical_formula, data source and exact mass
 #' \describe{
 #'   \item{name}{compounds name}
@@ -89,12 +85,10 @@
 
 #' A list containing HMDB GC-EI-MS spectra database
 #' @docType data
-#' @usage data(hmdbcms)
 #' @format A list with compounds from hmdb for GC-MS simulation
 "hmdbcms"
 
 #' A list containing MoNA LC-MS spectra database
 #' @docType data
-#' @usage data(monams1)
 #' @format A list with compounds from MoNA LC-MS spectra database for GC-MS simulation
 "monams1"
